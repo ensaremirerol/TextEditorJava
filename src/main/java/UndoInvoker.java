c8d7b3a7971dfc68a3362c01557de492d342c3c9
Ensar Emir EROL <ensaremir.erol99@gmail.com>
2021-01-26 02:54:43 +0300

    Mementos implemented

--- src/main/java/UndoInvoker.java
+++ src/main/java/UndoInvoker.java
@@ -1,5 +1,10 @@
 
-import javax.swing.undo.UndoManager;
+import javax.swing.JTextArea;
+import javax.swing.event.UndoableEditEvent;
+import javax.swing.event.UndoableEditListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -11,46 +16,89 @@ import javax.swing.undo.UndoManager;
  *
  * @author ensar
  */
-public class UndoInvoker{
-    final private Command undo;
-    final private Command redo;
-    public UndoInvoker(UndoManager undoManager){
-        undo = new UndoCommand(undoManager);
-        redo = new RedoCommand(undoManager);
+public class UndoInvoker implements UndoableEditListener{
+    private boolean triggerListener = true;
+    final private Command undo, redo;
+    final private UndoCareTaker undoMemento, redoMemento;
+    final private UndoOrginator orginator;
+    final private JTextArea textArea;
+    public UndoInvoker(JTextArea textArea){
+        this.textArea = textArea;
+        undoMemento = new UndoCareTaker();
+        redoMemento = new UndoCareTaker();
+        orginator = new UndoOrginator(textArea.getText());
+        undo = new UndoCommand(undoMemento, redoMemento, orginator);
+        redo = new RedoCommand(undoMemento, redoMemento, orginator);
+        
     }
     
     public void undo(){
+        triggerListener = false;
         undo.execute();
+        textArea.setText(orginator.saveMementos());
+        triggerListener = true;
     }
     
     public void redo(){
+        triggerListener = false;
         redo.execute();
+        textArea.setText(orginator.saveMementos());
+        triggerListener = true;
+    }
+
+    @Override
+    public void undoableEditHappened(UndoableEditEvent e) {
+        if(triggerListener){
+            Document obj = (Document) e.getSource();
+            try{
+                undoMemento.addMementos(obj.getText(0, obj.getLength() - 1));
+            }
+            catch(BadLocationException a){}
+            orginator.setMementos(textArea.getText());
+        }
+        
     }
     
 }
 
 class UndoCommand implements Command{
-    final private UndoManager undoManager;
+    final private UndoCareTaker undoMemento, redoMemento;
+    final private UndoOrginator orginator;
 
-    public UndoCommand(UndoManager undoManager) {
-        this.undoManager = undoManager;
+    public UndoCommand(UndoCareTaker undoMementos, UndoCareTaker redoMementos, UndoOrginator orginator) {
+        this.undoMemento = undoMementos;
+        this.redoMemento = redoMementos;
+        this.orginator = orginator;
     }
     
     @Override
     public void execute(){
-        if(undoManager.canUndo()) undoManager.undo();
+        try {
+            String memento = undoMemento.getMementos();
+            orginator.setMementos(memento);
+            redoMemento.addMementos(memento);
+        } catch (MementosIsEmpty e) {
+        }
     }
 }
 
 class RedoCommand implements Command{
-    final private UndoManager undoManager;
+    final private UndoCareTaker undoMemento, redoMemento;
+    final private UndoOrginator orginator;
 
-    public RedoCommand(UndoManager undoManager) {
-        this.undoManager = undoManager;
+    public RedoCommand(UndoCareTaker undoMementos, UndoCareTaker redoMementos, UndoOrginator orginator) {
+        this.undoMemento = undoMementos;
+        this.redoMemento = redoMementos;
+        this.orginator = orginator;
     }
     
     @Override
     public void execute(){
-        if(undoManager.canRedo()) undoManager.redo();
+        try {
+            String memento = redoMemento.getMementos();
+            orginator.setMementos(memento);
+            undoMemento.addMementos(memento);
+        } catch (MementosIsEmpty e) {
+        }
     }
 }
